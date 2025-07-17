3cbace57ce13c06ea6273fa5f35ededa36ee2131
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-07-17 17:20:21 +0500

    changes to main

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from agents.mcp import MCPServerStdio
+from agents.mcp import MCPServerSse
 from openai import AsyncOpenAI
 from agents import Agent, Runner, OpenAIChatCompletionsModel
 from dotenv import load_dotenv
 import os 
-load_dotenv()
 import asyncio
 
-
-async def create_agent(server):
-    """Create an agent."""
-    client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # OpenAI key
-    model = OpenAIChatCompletionsModel(model="gpt-4o", openai_client=client)
-    return Agent(name="AI", instructions="You are helpful", model=model, mcp_servers=[server])
+load_dotenv()
 
 
 async def main():
-    """Run the main program."""
-    async with MCPServerStdio(name="playwright", params={"command": "playwright-server"}) as server:
-        agent = await create_agent(server)
-        query = "navigate to https://www.google.com and take a screenshot named 'google.png'"
-        out = await Runner.run(agent, input=query)
-        print(out.final_output)
-
+    async with MCPServerSse(name="playwright", params={
+        "url": "http://localhost:8931/sse",
+        "transport": "sse"
+    }) as server:
+        client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # OpenAI key
+        model = OpenAIChatCompletionsModel(model="gpt-4o", openai_client=client)
+        agent = Agent(name="AI", instructions="You can browse with Playwright", model=model, mcp_servers=[server])
+        print((await Runner.run(agent, input="Go to https://www.google.com, find the search field, click on the search field and add text 'hello' and press enter and print the first link and heading")).final_output)
 
 if __name__ == "__main__":
     asyncio.run(main())
