d8194c70e3a48c50495c3a01a3bad12d1fb9ef4c
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-07-18 18:39:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ load_dotenv()
 async def main():
     async with MCPServerSse(name="playwright", params={
         "url": "http://localhost:8931/sse",
-        "transport": "sse"
+        "transport": "sse",
+        "timeout": 300
     }) as server:
         client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # OpenAI key
         model = OpenAIChatCompletionsModel(model="gpt-4o", openai_client=client)
         agent = Agent(name="AI", instructions="You can browse with Playwright", model=model, mcp_servers=[server])
-        print((await Runner.run(agent, input="Go to https://www.google.com, find the search field, click on the search field and add text 'hello' and press enter and print the first link and heading")).final_output)
+        # print((await Runner.run(agent, input="Go to https://www.google.com, find the search field, click on the search field and add text 'hello' and press enter and print the first link and heading")).final_output)
+        print((await Runner.run(agent, input="Go to https://www.einfach-sparsam.de/gewinnspiele, find the competitions, print the Heading and Competition links")).final_output)
 
 if __name__ == "__main__":
     asyncio.run(main())
